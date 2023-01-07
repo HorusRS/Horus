d1370663efff8214795d70ac13b344338f810839
Noam Eliyahu <noam.e6102004@gmail.com>
2023-01-07 12:32:24 +0200

    @liamsa1212 Just for you

---
+++ root/src/agent_config_helper.rs
@@ -0,0 +1,37 @@
+#[allow(non_camel_case_types)]
+
+pub mod toml_config{
+    use serde_derive::{Deserialize, Serialize};
+    use std::fs;
+    use std::process::exit;
+    use toml;
+    // Top level struct to hold the TOML data.
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct Config_t {
+        signatures: Vec<Signature_t>,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct Signature_t {
+        pub alert_name: String,
+        pub pattern_type: String,
+        pub pattern_data: String,
+    }
+    pub fn load_agent_config(path: &str) -> Config_t {
+        let file_str = match fs::read_to_string(path) {
+            Ok(c) => c,
+            Err(_) => {
+                eprintln!("Could not read file `{}`", path);
+                exit(1);
+            }
+        };
+        let data: Config_t = toml::from_str(&file_str).unwrap();
+        return data;
+    }
+
+    pub fn load_to_signatures(data: Vec<Signature_t>) {
+        let toml_str = toml::to_string(&data).unwrap();
+        fs::write("test.toml", "[config]\n".to_owned() + &toml_str).unwrap();
+    }
+}
